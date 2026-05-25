628f622be5bdcb379cf0977b99d75affe9e89742
Salaj_07 <salajchaudhary2001@gmail.com>
2026-05-25 12:32:30 +0545

    feat: add book repair page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
+import BookRepair from './pages/BookRepair'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/book" element={<BookRepair />} />
       </Routes>
     </BrowserRouter>
   )
