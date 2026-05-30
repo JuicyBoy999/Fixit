7f611fa50c236068fb6ed73bc30da6a9357bdd77
Salaj_07 <salajchaudhary2001@gmail.com>
2026-05-30 11:57:16 +0545

    feat: browse technicians and availability calendar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Signup from './pages/Signup'
-import Profile from './pages/Profile'
-import BookRepair from './pages/BookRepair'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Dashboard from './pages/Dashboard';
+import BookRepair from './pages/BookRepair';
+import BrowseTechnicians from './pages/BrowseTechnicians';
+import TechnicianCalendar from './pages/TechnicianCalendar';
+import { AuthProvider } from './context/AuthContext';
+import AdminLogin from './components/auth/AdminLogin';
+import RequestPasswordReset from './pages/RequestPasswordReset';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/book" element={<BookRepair />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/forgot-password" element={<RequestPasswordReset />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/book" element={<BookRepair />} />
+          <Route path="/technicians" element={<BrowseTechnicians />} />
+          <Route path="/calendar" element={<TechnicianCalendar />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
